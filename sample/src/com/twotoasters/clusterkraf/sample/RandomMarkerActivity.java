f0ecc200f2443b6ce4f76f0e20331a8df38217b4
Carlton Whitehead <carlton@twotoasters.com>
2013-04-16 14:39:20 -0400

    Implement custom marker assets

--- sample/src/com/twotoasters/clusterkraf/sample/RandomMarkerActivity.java
+++ sample/src/com/twotoasters/clusterkraf/sample/RandomMarkerActivity.java
@@ -4,9 +4,16 @@ import java.io.Serializable;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -213,6 +220,25 @@ public class RandomMarkerActivity extends FragmentActivity implements GenerateRa
 
 	public class ToastedMarkerOptionsChooser extends MarkerOptionsChooser {
 
+		private final Paint clusterPaintLarge;
+		private final Paint clusterPaintMedium;
+		private final Paint clusterPaintSmall;
+
+		public ToastedMarkerOptionsChooser() {
+			clusterPaintMedium = new Paint();
+			clusterPaintMedium.setColor(Color.WHITE);
+			clusterPaintMedium.setAlpha(255);
+			clusterPaintMedium.setTextAlign(Paint.Align.CENTER);
+			clusterPaintMedium.setTypeface(Typeface.create(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC));
+			clusterPaintMedium.setTextSize(getResources().getDimension(R.dimen.cluster_text_size_medium));
+
+			clusterPaintSmall = new Paint(clusterPaintMedium);
+			clusterPaintSmall.setTextSize(getResources().getDimension(R.dimen.cluster_text_size_small));
+
+			clusterPaintLarge = new Paint(clusterPaintMedium);
+			clusterPaintLarge.setTextSize(getResources().getDimension(R.dimen.cluster_text_size_large));
+		}
+
 		/*
 		 * (non-Javadoc)
 		 * 
@@ -229,25 +255,59 @@ public class RandomMarkerActivity extends FragmentActivity implements GenerateRa
 			String title;
 			if (isCluster) {
 				title = getResources().getQuantityString(R.plurals.count_points, clusterPoint.size(), clusterPoint.size());
+				int clusterSize = clusterPoint.size();
 				if (hasTwoToasters) {
-					icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE);
+					icon = BitmapDescriptorFactory.fromBitmap(getClusterBitmap(R.drawable.ic_map_pin_cluster_toaster, clusterSize));
 					title = getString(R.string.including_two_toasters, title);
 				} else {
-					icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN);
-					title = getResources().getQuantityString(R.plurals.count_points, clusterPoint.size(), clusterPoint.size());
+					icon = BitmapDescriptorFactory.fromBitmap(getClusterBitmap(R.drawable.ic_map_pin_cluster, clusterSize));
+					title = getResources().getQuantityString(R.plurals.count_points, clusterSize, clusterSize);
 				}
 			} else {
 				MarkerData data = (MarkerData)clusterPoint.getPointAtOffset(0).getTag();
 				if (hasTwoToasters) {
-					icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED);
+					icon = BitmapDescriptorFactory.fromResource(R.drawable.ic_map_pin_toaster);
 					title = data.getLabel();
 				} else {
-					icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE);
+					icon = BitmapDescriptorFactory.fromResource(R.drawable.ic_map_pin);
 					title = getString(R.string.point_number_x, data.getLabel());
 				}
 			}
-			markerOptions.title(title);
 			markerOptions.icon(icon);
+			markerOptions.title(title);
+			markerOptions.anchor(0.5f, 1.0f);
+		}
+
+		@SuppressLint("NewApi")
+		private Bitmap getClusterBitmap(int resourceId, int clusterSize) {
+			BitmapFactory.Options options = new BitmapFactory.Options();
+
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+				options.inMutable = true;
+			}
+			Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resourceId, options);
+			if (bitmap.isMutable() == false) {
+				bitmap = bitmap.copy(bitmap.getConfig(), true);
+			}
+
+			Canvas canvas = new Canvas(bitmap);
+
+			Paint paint = null;
+			float originY;
+			if (clusterSize < 100) {
+				paint = clusterPaintLarge;
+				originY = bitmap.getHeight() * 0.64f;
+			} else if (clusterSize < 1000) {
+				paint = clusterPaintMedium;
+				originY = bitmap.getHeight() * 0.6f;
+			} else {
+				paint = clusterPaintSmall;
+				originY = bitmap.getHeight() * 0.56f;
+			}
+
+			canvas.drawText(String.valueOf(clusterSize), bitmap.getWidth() * 0.5f, originY, paint);
+
+			return bitmap;
 		}
 	}
 
